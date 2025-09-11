3de8af7b702fba544ba9634a9827c9bcf6cb32a0
hexdhog <hexdhog@proton.me>
2025-09-11 12:34:13 +0200

    added test sum with 4 dims compared with pytorch output

--- test.c
+++ test.c
@@ -104,8 +104,48 @@ const char *test_sum_dim_out_of_range() {
     return __func__;
 }
 
+const char *test_sum_dim4() {
+    tensor_t *t = tensor_alloc(4, (uint16_t[]){2, 3, 2, 4});
+    for (uint32_t i = 0; i < t->nelem; i++) t->data[i] = i + 1;
+
+    // comparing results with pytorch's
+    {
+        float expected[] = { 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72 };
+        uint32_t nelem = sizeof(expected) / sizeof(*expected);
+        tensor_t *r = sum(t, 0, false);
+        assert(r->nelem == nelem);
+        for (uint32_t i = 0; i < nelem; i++) assert(expected[i] == r->data[i]);
+    }
+
+    {
+        float expected[] = { 27,  30,  33,  36,  39,  42,  45,  48,  99, 102, 105, 108, 111, 114, 117, 120 };
+        uint32_t nelem = sizeof(expected) / sizeof(*expected);
+        tensor_t *r = sum(t, 1, false);
+        assert(r->nelem == nelem);
+        for (uint32_t i = 0; i < nelem; i++) assert(expected[i] == r->data[i]);
+    }
+
+    {
+        float expected[] = { 6,  8, 10, 12, 22, 24, 26, 28, 38, 40, 42, 44, 54, 56, 58, 60, 70, 72, 74, 76, 86, 88, 90, 92 };
+        uint32_t nelem = sizeof(expected) / sizeof(*expected);
+        tensor_t *r = sum(t, 2, false);
+        assert(r->nelem == nelem);
+        for (uint32_t i = 0; i < nelem; i++) assert(expected[i] == r->data[i]);
+    }
+
+    {
+        float expected[] = { 10,  26,  42,  58,  74,  90, 106, 122, 138, 154, 170, 186 };
+        uint32_t nelem = sizeof(expected) / sizeof(*expected);
+        tensor_t *r = sum(t, 3, false);
+        assert(r->nelem == nelem);
+        for (uint32_t i = 0; i < nelem; i++) assert(expected[i] == r->data[i]);
+    }
+
+    return __func__;
+}
+
 const char *(*fnx[])(void) = {
-    test_sumall, test_sum_dim0, test_sum_dim1_keepdim, test_sum_negative_dim, test_sum_dim_out_of_range
+    test_sumall, test_sum_dim0, test_sum_dim1_keepdim, test_sum_negative_dim, test_sum_dim_out_of_range, test_sum_dim4
 };
 
 int main(int argc, char **argv) {
